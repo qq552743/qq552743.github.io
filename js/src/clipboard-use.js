8bb1599c9b7c44a2da0c1d736c9c09ac406312ea
qq552743 <rockinghl@163.com>
2019-03-17 23:54:59 +0800

    Site updated: 2019-03-17 23:54:37

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,19 +1,3 @@
-  /*页面载入完成后，创建复制按钮*/
-  /*页https://yfzhou.coding.me/2018/08/27/Hexo-Next搭建个人博客%EF%BC%88代码块复制功能%EF%BC%89/*/
-  !function (e, t, a) {
-    /* code */
-    var initCopyCode = function(){
-      var copyHtml = '';
-      copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-      //fa fa-globe可以去字体库替换自己想要的图标
-copyHtml += '  <i class="fa fa-clipboard"></i><span>copy</span>';
-      copyHtml += '</button>';
-      $(".highlight .code pre").before(copyHtml);
-      new ClipboardJS('.btn-copy', {
-          target: function(trigger) {
-              return trigger.nextElementSibling;
-          }
-      });
-    }
-    initCopyCode();
-  }(window, document);
+// build time:Sun Mar 17 2019 23:54:35 GMT+0800 (GMT+08:00)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
