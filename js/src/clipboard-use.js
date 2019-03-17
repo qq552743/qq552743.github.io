50bc6d084315866c6d5afeba8a1e622d784e0417
qq552743 <rockinghl@163.com>
2019-03-18 00:30:30 +0800

    Site updated: 2019-03-18 00:30:16

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Mar 17 2019 23:54:35 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 18 2019 00:30:03 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
