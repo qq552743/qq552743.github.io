9e336a6210591b3f9cc649001f8d5e1a66c27add
qq552743 <rockinghl@163.com>
2019-04-23 18:45:45 +0800

    Site updated: 2019-04-23 18:45:33

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 13 2019 18:14:55 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 23 2019 18:44:54 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
--- js/src/custom/custom.js
+++ js/src/custom/custom.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 13 2019 18:14:55 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 23 2019 18:44:54 GMT+0800 (GMT+08:00)
 $(document).ready(function(){$(window).scroll(function(){$(".top-scroll-bar").attr("style","width: "+$(this).scrollTop()/($(document).height()-$(this).height())*100+"%; display: block;");var c=$(window).scrollTop();var o=$(document).height();var r=$(window).height();var s=parseInt(c/(o-r)*100);$(".top-scroll-bar").css("width",s+"%");if(s>=0&&s<=19)$(".top-scroll-bar").css("background","#cccccc");if(s>=20&&s<=39)$(".top-scroll-bar").css("background","#50bcb6");if(s>=40&&s<=59)$(".top-scroll-bar").css("background","#85c440");if(s>=60&&s<=79)$(".top-scroll-bar").css("background","#f2b63c");if(s>=80&&s<=99)$(".top-scroll-bar").css("background","#FF0000");if(s==100)$(".top-scroll-bar").css("background","#f58ca1")})});
 //rebuild by neat 
